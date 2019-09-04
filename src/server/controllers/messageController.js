a70a7f0e2601027de4fd102cfb09e8ff62dd9e43
Geoffrey Lin <geoffrey.s.lin@gmail.com>
2019-09-03 20:53:44 -0400

    Set up template skeleton for messageController

--- src/server/controllers/messageController.js
+++ src/server/controllers/messageController.js
@@ -1,3 +1,18 @@
 module.exports = {
+  getMessages: (req, res) => {
+
+  },
+
+  postMessage: (req, res) => {
+
+  },
+
+  editMessage: (req, res) => {
+
+  },
+
+  deleteMessage: (req, res) => {
+
+  },
   
 };
