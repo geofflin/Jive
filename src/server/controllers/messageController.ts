ff655139323bec8ba08a51bd3a024933e1ec6007
Geoffrey Lin <geoffrey.s.lin@gmail.com>
2019-09-06 13:29:33 -0400

    Converted messageController to TypeScript template

---
+++ src/server/controllers/messageController.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express';
+
+export default {
+  getMessages: (req: Request, res: Response, next: NextFunction ) => {
+
+  },
+
+  addMessage: (req: Request, res: Response, next: NextFunction ) => {
+
+  },
+
+  editMessage: (req: Request, res: Response, next: NextFunction ) => {
+
+  },
+
+  deleteMessage: (req: Request, res: Response, next: NextFunction ) => {
+
+  },
+  
+};
