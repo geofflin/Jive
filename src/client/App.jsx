23112c074614bc90366ee501aedd28d2c345820c
Geoffrey Lin <geoffrey.s.lin@gmail.com>
2019-09-06 11:18:30 -0400

    Dummy websockets working initial attempt

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
 const App = () => {
+  const url = 'ws://localhost:3000';
+  const socket = new WebSocket(url);
+  socket.addEventListener('open', () => {
+    socket.send('Hello World!');
+  });
+   
+  socket.addEventListener('message', event => {
+    console.log(`Message from server: ${event.data}`);
+  });
   return (
     <div>
       <h2>REACT APP COMPONENT</h2>
--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,5 @@
+const WebSocket = require('ws');
+const http = require('http');
 const express = require('express'); 
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -5,6 +7,8 @@ const cookieParser = require('cookie-parser');
 const messageRouter = require('./routers/messageRouter');
 
 const app = express();
+const server = http.createServer(app);
+// const wss = new WebSocket.Server({ server });
 const PORT = 3000;
 
 app.use(bodyParser.json());
@@ -18,4 +22,20 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use('/messages', messageRouter);
 
-app.listen(PORT, () => console.log(`App is now listening on ${PORT}`));
+// server.listen(3001, () => {
+//   console.log(`Server started on port ${server.address().port}`);
+// });
+
+
+const wss = new WebSocket.Server({ 
+  server: app.listen(PORT, () => console.log(`App is now listening on ${PORT}`))
+});
+
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log('received: %s', message);
+    ws.send(`Hello, you sent -> ${message}`);
+  });
+
+  ws.send('Hi there, I am a WebSocket server');
+});
